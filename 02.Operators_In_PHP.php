640b8eda330b23d5531268d9321554cc043fc437
Sohelsh <sohelshaikhadam@gmail.com>
2022-10-15 22:36:14 +0530

    Program In PHP

---
+++ 02.Operators_In_PHP.php
@@ -0,0 +1,30 @@
+<?php
+  // Concatention Operators(.)
+ 
+  $Str1 = "Hello";
+  $Str2 = "Welcome";
+  
+  echo $Str1." ".$Str2;
+
+ // Arithmetic Operators(+,-,%,/)
+ 
+ $No1 = 30;
+ $No2 = 20;
+
+ $Sum = $No1 + $No2 ;
+
+ echo "<br>Addtion Of Two Numbers = $Sum";
+
+ // Comparison Operators(==,===)
+
+  $No = 10;
+  
+  if($No % 2 == 0)
+  {
+    echo "<br>Number Is Even";
+  }
+  else
+  {
+    echo "Number Is Odd";
+  }
+?>
